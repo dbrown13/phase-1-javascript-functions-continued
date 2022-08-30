04ed5133ca3fe6fa7ff27cd566de8f6136db4552
Deryn Brown <Deryn.2017@gmail.com>
2022-08-29 22:35:42 -0500

    commit tree 4a173b99c6936eeef3a255de5af23b8d011462fb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+
+}
+
+function wrapAdjective(flair = '*') { 
+    return function(wrap = "special") {
+        return `You are ${flair}${wrap}${flair}!`;
+    }
+
+}
